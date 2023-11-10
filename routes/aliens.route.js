3b5cf38d0bb491ef2299aca79c7e579ceb695461
Emilee Hatch <121782677+EmToHat@users.noreply.github.com>
2023-11-09 22:58:12 -0700

    added validation to aliens.route.js

--- routes/aliens.route.js
+++ routes/aliens.route.js
@@ -1,22 +1,41 @@
-const express = require("express"); // Import the Express framework
-const router = express.Router(); // Create an instance of the Express Router
-const alienController = require("../controllers/aliens.controller"); // Import the Alien controller
+const express = require("express");
+const { validationResult } = require("express-validator");
+const alienController = require("../controllers/aliens.controller");
+const { createAlienValidation, updateAlienValidation, deleteAlienValidation } = require("../validation/aliens.validator");
+
+// Middleware for handling validation errors
+const validate = (req, res, next) => {
+    // Check for validation errors using express-validator
+    const errors = validationResult(req);
+
+    // If there are validation errors, return a 400 Bad Request response with error details
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+  // If there are no validation errors, proceed to the next middleware or route handler
+    next();
+};
+
+// Create an instance of the Express Router
+const router = express.Router();
 
 // Define routes for Aliens
 
-// Route for creating a new Alien
-router.post("/", alienController.createAlien);
+// Route for creating a new Alien with validation
+router.post("/", createAlienValidation, validate, alienController.createAlien);
 
 // Route for getting a list of all Aliens
 router.get("/", alienController.getAllAliens);
 
-// Route for getting a specific Alien by ID
-router.get("/:id", alienController.getAlienById);
+// Route for getting a specific Alien by ID with validation
+router.get("/:id", validate, alienController.getAlienById);
 
-// Route for updating an existing Alien by ID
-router.put("/:id", alienController.updateAlien);
+// Route for updating an existing Alien by ID with validation
+router.put( "/:id", updateAlienValidation, validate, alienController.updateAlien);
 
-// Route for deleting an Alien by ID
-router.delete("/:id", alienController.deleteAlien);
+// Route for deleting an Alien by ID with validation
+router.delete("/:id", deleteAlienValidation, validate, alienController.deleteAlien);
 
-module.exports = router; // Export the router for use in other parts of the application
+// Export the router for use in other parts of the application
+module.exports = router;
