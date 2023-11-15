a7b3af7cfa947fcf34dcfe75d8744e74bfbaef52
Emilee Hatch <121782677+EmToHat@users.noreply.github.com>
2023-11-14 21:47:25 -0700

    removed express-validator

--- routes/aliens.route.js
+++ routes/aliens.route.js
@@ -1,41 +1,50 @@
 const express = require("express");
-const { validationResult } = require("express-validator");
 const alienController = require("../controllers/aliens.controller");
-const { createAlienValidation, updateAlienValidation, deleteAlienValidation } = require("../validation/aliens.validator");
+const { createAlienSchema, updateAlienSchema, deleteAlienSchema, getAlienByIdSchema, } = require("../validation/aliens.validator");
 
-// Middleware for handling validation errors
-const validate = (req, res, next) => {
-    // Check for validation errors using express-validator
-    const errors = validationResult(req);
+const router = express.Router();
 
-    // If there are validation errors, return a 400 Bad Request response with error details
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+// Middleware for handling Joi validation errors
+const validate = (schema, property) => (req, res, next) => {
+    const { error } = schema.validate(req[property], { abortEarly: false });
+
+    if (error) {
+        const errors = error.details.map((detail) => ({
+        field: detail.path[0],
+        message: detail.message,
+        }));
+
+        return res.status(400).json({ errors });
     }
 
-  // If there are no validation errors, proceed to the next middleware or route handler
     next();
 };
 
-// Create an instance of the Express Router
-const router = express.Router();
+// Middleware for handling 500 errors
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+};
 
 // Define routes for Aliens
 
 // Route for creating a new Alien with validation
-router.post("/", createAlienValidation, validate, alienController.createAlien);
+router.post("/", validate(createAlienSchema, "body"), alienController.createAlien);
 
 // Route for getting a list of all Aliens
 router.get("/", alienController.getAllAliens);
 
 // Route for getting a specific Alien by ID with validation
-router.get("/:id", validate, alienController.getAlienById);
+router.get("/:id",validate(getAlienByIdSchema, "params"),alienController.getAlienById);
 
 // Route for updating an existing Alien by ID with validation
-router.put( "/:id", updateAlienValidation, validate, alienController.updateAlien);
+router.put("/:id",validate(updateAlienSchema, "params"),alienController.updateAlien);
 
 // Route for deleting an Alien by ID with validation
-router.delete("/:id", deleteAlienValidation, validate, alienController.deleteAlien);
+router.delete("/:id",validate(deleteAlienSchema, "params"),alienController.deleteAlien);
+
+// Error handling middleware for 500 errors
+router.use(errorHandler);
 
 // Export the router for use in other parts of the application
 module.exports = router;
